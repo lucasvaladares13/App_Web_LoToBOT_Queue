6abc270a89b92406acb0c91499ca224544b81565
lucasvaladares13 <lucasvao96@gmail.com>
2023-04-09 20:16:06 -0300

    att

--- app.py
+++ app.py
@@ -19,16 +19,62 @@ logging.basicConfig(level=logging.ERROR)
 logging.getLogger().setLevel(logging.ERROR)
 logging.error('This is an error message')
 
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+# updater = Updater(token=TOKEN, use_context=True)
+# dispatcher = updater.dispatcher
 
 app.last_message = ''
 app.last_message_fila = ''
 
-from scripts.gerar_jogos import _gerar_jogos
+from azure.storage.queue import QueueClient
+from scripts.gerar_jogos import _gerar_jogos_fila
 from scripts.config import TOKEN, CANAL_ID
+
+import sqlalchemy as db
+import urllib
+import time
+
+# Armazena o tempo inicial
+start_time = time.time()
+sql = db.text('SELECT * FROM base')
+
+connection_string = 'mssql+pyodbc:///?odbc_connect={}'.format(urllib.parse.quote_plus(os.environ['DATABASE_URL']))
+engine = db.create_engine(connection_string, echo=True)
+connection = engine.connect()
+
+RESULT = connection.execute(sql).fetchall()
+LEN_RESULT = len(RESULT)
+connection.close()
+end_time = time.time()
+
+# Calcula o tempo de execução
+TEMPO_EXECUCAO = end_time - start_time
+
+
+
     # Adicione um manipulador de mensagens para receber mensagens de texto
-dispatcher.add_handler(MessageHandler(Filters.chat(CANAL_ID), _gerar_jogos))
+# dispatcher.add_handler(MessageHandler(Filters.chat(CANAL_ID), _gerar_jogos))
+
+# url_queue = 'https://stappweblotobot.queue.core.windows.net/mesagens-bot'
+# token_sas = '?sv=2021-12-02&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2025-04-08T03:27:10Z&st=2023-04-07T19:27:10Z&spr=https,http&sig=pMMG9sZ32h3ITso%2B7Rovj0Fujr24LC%2FqHBT2RZrwcgo%3D'
+
+# queue_service = QueueClient.from_queue_url(
+#        queue_url=url_queue,
+#        credential=token_sas
+#    )
+
+# import json
+
+# while 1:
+#     messages = queue_service.receive_messages(max_messages = 1, visibility_timeout = 600)
+#     for message in messages:
+#         message_text = message.content
+#         print(message_text)
+#         status = _gerar_jogos_fila(message_text)
+#         print(status)
+#         if status == 'Arquivo enviado!':
+#             queue_service.delete_message(message)
+
+        
 
 
 
@@ -53,32 +99,40 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
-@app.route(f'/{TOKEN}', methods=['POST'])
-def recive_message():
-   print('ok')
-   json_data = request.get_json()
-   update = Update.de_json(json_data, updater.bot)
-   dispatcher.process_update(update)
+# @app.route(f'/{TOKEN}', methods=['POST'])
+# def recive_message():
+#    print('ok')
+#    json_data = request.get_json()
+#    update = Update.de_json(json_data, updater.bot)
+#    dispatcher.process_update(update)
     
-   app.last_message = str(json_data)
-   return 'ok'
+#    app.last_message = str(json_data)
+#    return 'ok'
  
-@app.route('/mensagem')
-def mensagem():
-    return render_template('mensagem.html', mensagem=app.last_message)
+# @app.route('/mensagem')
+# def mensagem():
+#     return render_template('mensagem.html', mensagem=app.last_message)
 
-@app.route('/receberfila', methods=['POST'])
-def receber_fila():
-    json_data = request.get_json()
-    app.last_message_fila = json_data
-    return '200'
+# @app.route('/receberfila', methods=['POST'])
+# def receber_fila():
+#     json_data = request.get_json()
+#     app.last_message_fila = json_data
+#     return '200'
 
 
 
 @app.route('/mensagemfila')
 def mensagem_fila():
-    
-    return render_template('mensagem.html', mensagem=app.last_message_fila)
+   
+
+    import psutil
+
+    process = psutil.Process()
+    mem_info = process.memory_info()
+
+    print(f"")
+
+    return render_template('mensagem.html', mensagem=f'Tempo de execução: {TEMPO_EXECUCAO} \n Tamanho da tabela: {LEN_RESULT}\n Consumo de memória RAM: {mem_info.rss / 1024 / 1024:.2f} MB')
 
 
 
--- scripts/gerar_jogos/__init__.py
+++ scripts/gerar_jogos/__init__.py
@@ -55,7 +55,67 @@ def _gerar_jogos(update, context):
 
     data = date.today()
     data_em_texto = data.strftime('%d_%m_%Y')
+    
     context.bot.send_document(chat_id=CANAL_ID, document=output, filename=f'{context_dict["user_id"]}-Jogos_LotoFacil_{data_em_texto}.xlsx')
+
+def _gerar_jogos_fila(mensagem):
+    import telegram
+    from scripts.config import TOKEN, CANAL_ID
+
+    context_dict = init_user_data()
+    
+    print(mensagem)
+    mensagem = mensagem.split(';')
+    print(mensagem)
+    context_dict['user_id'] = mensagem[0]
+    context_dict['Dezenas_Fixas'] = [] if mensagem[1] == '[]' else mensagem[1].replace("[","").replace("]","").replace(" ","").split(',')
+    context_dict['Par_Impar'] = mensagem[2]
+    context_dict['Flag_Ultimo_jogo'] = mensagem[3]
+    context_dict['Qtd_Jogos'] = mensagem[4]
+
+    
+    qtd_jogos = context_dict['Qtd_Jogos']
+    if qtd_jogos != 10:
+        qtd_jogos = qtd_jogos.replace('Qtd: ','')
+    sql = _get_sql_query_text(context_dict)
+    sql = db.text(sql)
+
+    connection_string = 'mssql+pyodbc:///?odbc_connect={}'.format(urllib.parse.quote_plus(os.environ['DATABASE_URL']))
+    engine = db.create_engine(connection_string, echo=True)
+    connection = engine.connect()
+
+    result = connection.execute(sql).fetchall()
+    connection.close()
+
+    df = pd.DataFrame(result, columns=['D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'D11', 'D12', 'D13', 'D14', 'D15', 'CHAVE'])
+
+
+
+    
+    df = df.sample(int(qtd_jogos))
+    
+    #df = export_user_data(context)
+    # Cria um objeto BytesIO para armazenar o arquivo em memória
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
+    writer.save()
+    output.seek(0)
+
+    status = 1
+    while status == 1:
+        try:
+            data = date.today()
+            data_em_texto = data.strftime('%d_%m_%Y')
+            bot = telegram.Bot(token=TOKEN)
+
+            # Envia a mensagem de teste
+            bot.send_document(chat_id=CANAL_ID, document=output, filename=f'{context_dict["user_id"]}-Jogos_LotoFacil_{data_em_texto}.xlsx')
+            status = 0
+        except:
+            print( 'Tentando enviar denovo!')
+    
+    return 'Arquivo enviado!'
 
 
 
