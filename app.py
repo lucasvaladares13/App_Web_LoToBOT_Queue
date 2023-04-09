627699556be4dcace0ed6dbd27ee4e2739cda141
lucasvaladares13 <lucasvao96@gmail.com>
2023-04-09 20:38:55 -0300

    att

--- app.py
+++ app.py
@@ -33,46 +33,30 @@ import sqlalchemy as db
 import urllib
 import time
 
-# Armazena o tempo inicial
-start_time = time.time()
-sql = db.text('SELECT * FROM base')
-
-connection_string = 'mssql+pyodbc:///?odbc_connect={}'.format(urllib.parse.quote_plus(os.environ['DATABASE_URL']))
-engine = db.create_engine(connection_string, echo=True)
-connection = engine.connect()
-
-RESULT = connection.execute(sql).fetchall()
-LEN_RESULT = len(RESULT)
-connection.close()
-end_time = time.time()
-
-# Calcula o tempo de execução
-TEMPO_EXECUCAO = end_time - start_time
-
 
 
     # Adicione um manipulador de mensagens para receber mensagens de texto
 # dispatcher.add_handler(MessageHandler(Filters.chat(CANAL_ID), _gerar_jogos))
 
-# url_queue = 'https://stappweblotobot.queue.core.windows.net/mesagens-bot'
-# token_sas = '?sv=2021-12-02&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2025-04-08T03:27:10Z&st=2023-04-07T19:27:10Z&spr=https,http&sig=pMMG9sZ32h3ITso%2B7Rovj0Fujr24LC%2FqHBT2RZrwcgo%3D'
+url_queue = 'https://stappweblotobot.queue.core.windows.net/mesagens-bot'
+token_sas = '?sv=2021-12-02&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2025-04-08T03:27:10Z&st=2023-04-07T19:27:10Z&spr=https,http&sig=pMMG9sZ32h3ITso%2B7Rovj0Fujr24LC%2FqHBT2RZrwcgo%3D'
 
-# queue_service = QueueClient.from_queue_url(
-#        queue_url=url_queue,
-#        credential=token_sas
-#    )
+queue_service = QueueClient.from_queue_url(
+       queue_url=url_queue,
+       credential=token_sas
+   )
 
-# import json
+import json
 
-# while 1:
-#     messages = queue_service.receive_messages(max_messages = 1, visibility_timeout = 600)
-#     for message in messages:
-#         message_text = message.content
-#         print(message_text)
-#         status = _gerar_jogos_fila(message_text)
-#         print(status)
-#         if status == 'Arquivo enviado!':
-#             queue_service.delete_message(message)
+while 1:
+    messages = queue_service.receive_messages(max_messages = 1, visibility_timeout = 600)
+    for message in messages:
+        message_text = message.content
+        print(message_text)
+        status = _gerar_jogos_fila(message_text)
+        print(status)
+        if status == 'Arquivo enviado!':
+            queue_service.delete_message(message)
 
         
 
@@ -98,43 +82,6 @@ def hello():
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
-
-# @app.route(f'/{TOKEN}', methods=['POST'])
-# def recive_message():
-#    print('ok')
-#    json_data = request.get_json()
-#    update = Update.de_json(json_data, updater.bot)
-#    dispatcher.process_update(update)
-    
-#    app.last_message = str(json_data)
-#    return 'ok'
- 
-# @app.route('/mensagem')
-# def mensagem():
-#     return render_template('mensagem.html', mensagem=app.last_message)
-
-# @app.route('/receberfila', methods=['POST'])
-# def receber_fila():
-#     json_data = request.get_json()
-#     app.last_message_fila = json_data
-#     return '200'
-
-
-
-@app.route('/mensagemfila')
-def mensagem_fila():
-   
-
-    import psutil
-
-    process = psutil.Process()
-    mem_info = process.memory_info()
-
-    print(f"")
-
-    return render_template('mensagem.html', mensagem=f'Tempo de execução: {TEMPO_EXECUCAO} \n Tamanho da tabela: {LEN_RESULT}\n Consumo de memória RAM: {mem_info.rss / 1024 / 1024:.2f} MB')
-
-
 
 
 if __name__ == '__main__':
